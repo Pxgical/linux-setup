3e1727249abf3f3a7df59af066ec37b3accb1a0e
pugical <167028285+Pxgical@users.noreply.github.com>
2024-08-02 14:38:20 +0100

    Made script more modular and added nerd-fonts

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
+function command_exists() {
+	which "$1" >/dev/null 2>&1
+}
 function check_install() {
 	#Checks whether package is installed locally using pacman
 	if pacman -Qi "$1" &> /dev/null; then
 		echo "Package $1 is already installed..."
  	else
     		echo "Installing $1..."
-    		sudo pacman -S "$1"
+    		yes | sudo pacman -S "$1"
   	fi
 }
 function set_omz_update_option() {
@@ -33,36 +35,35 @@ function add_alias() {
 		echo "Setting $a_name alias..."
 		echo "alias $a_name=$a_val" >> ~/.zshrc
 	fi
-
 }
+
+
+
+
+
+
+
 function setup_paru() {
-	echo -e "\nStarting paru setup..."
-	#Checks whetehr the paru command exists
-	if ! command -v paru &> /dev/null; then
+	echo -e "\nSetting up paru..."
+	#Checks whether the paru command exists
+	if ! command_exists paru ; then
 		echo "Installing paru..."
-		check_install git
 		git clone --progress https://aur.archlinux.org/paru-bin.git
 		cd paru-bin
-		makepkg -si paru-bin
+		makepkg -si
 		cd ..
 		rm -rf paru-bin
+		echo "Paru finished installing..."
 	else 
 		echo "Paru already installed..."
 	fi
 }
-function setup_aliases() {
-	echo -e "\nStarting alias setup..."
-	
-	#Adds an alias to clean up unneeded packages
-	add_alias pacrubbish "'pacman -Qdtq | sudo pacman -Rns -'"
-}
 function setup_zsh() {
-	echo -e "\nStarting zsh setup..."
-	check_install zsh
+	echo -e "\nSetting up zsh..."
 
 	#Ensures the .zshrc file exists
 	if [ ! -f ~/.zshrc ]; then
-        	echo "Zshrc not found, creating now..."
+        	echo "~/.zshrc not found, creating now..."
 		touch ~/.zshrc
     	fi
 
@@ -70,29 +71,44 @@ function setup_zsh() {
 	if [ ! -d ~/.oh-my-zsh ]; then
  		echo "Installing oh-my-zsh..."
  		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
+		echo "oh-my-zsh finished installing..."
 	else
 		echo "oh-my-zsh already installed..."
 	fi
 
 	#Installs comfyline prompt
 	if [ ! -f ~/.oh-my-zsh/custom/themes/comfyline.zsh-theme ]; then
-		check_install git
-  		echo "Installing comfyline theme..."
+  		echo "Installing comfyline prompt..."
  		git clone https://gitlab.com/imnotpua/comfyline_prompt
   		cd comfyline_prompt
   		./install.sh
   		cd ..
   		rm -rf comfyline_prompt
 	else
-		echo "Comfyline already installed..."
+		echo "Comfyline prompt already installed..."
 	fi
 
-	#Sets oh-my-zsh's update mode to automatic
 	set_omz_update_option mode auto
-	#Sets oh-my-zsh's update frequency to weekly
 	set_omz_update_option frequency 7
 }
 
+echo -e "\nInstalling packages..."
+
+check_install git 
+check_install base-devel
+check_install zsh
+check_install trash-cli
+check_install curl
+check_install ttf-mononoki-nerd
+check_install ttf-nerd-fonts-symbols
+check_install ttf-nerd-fonts-symbols-mono
+
+echo -e "\nSetting up aliases..."
+	
+add_alias pacrubbish "'pacman -Qdtq | sudo pacman -Rns -'"
+add_alias pacrem "'pacman -R'"
+add_alias pacin "'sudo pacman -Sy'"
+
 setup_paru
 setup_zsh
-setup_aliases
+
